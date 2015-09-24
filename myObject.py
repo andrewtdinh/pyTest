7159a7a00698bc90b75518526b910984f2801a22
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-23 23:12:32 -0700

    Add comments to document the objects

--- myObject.py
+++ myObject.py
@@ -2,56 +2,55 @@ import unittest
 from myTags import myDict
 
 class Taggerizer:
-    #Define an updateable dictionary.  This is according to requirement 1.1
-    #tagDict = {'NAME': set(['Jack', 'Jill']), 'NUM': set(['one', 'two', 'three', 'nine'])}
     global tags
     tags = myDict()
 
     def __init__(self, inputString):
         self.originalStr = inputString
+
+        # The next two instance variables are sets because we want unique values inside
         self.taggedTerms = set()
         self.untaggedTerms = set()
+
+        # The next variable is a list because we want the terms to be in the order we add them
         self.replacedStr = []
+
+        # Convert everything in the string to lower case and split into list of terms
         terms = self.originalStr.lower().split(' ')
         for term in terms:
             foundKey = self.getDictKey(term)
+
+            # If a tag is found in our tag dictionary:
             if foundKey:
+                # We add the search term into the set of tags
                 self.taggedTerms.add(term)
+                # We add the returned key to the list of replaced string
                 self.replacedStr.append(foundKey)
             else:
+                # Else we just add the search term to the list of replaced string
                 self.replacedStr.append(term)
+                # And also add the search term into the set of non-tags
                 self.untaggedTerms.add(term)
 
-    #This will be used later to search for and get the key in the tagDict
+    # This will be used later to search for and get the key in the tagDict
     def getDictKey(self, term):
-        # for key, s in tagDict.items():
-        # for key, s in tags.tagDict.items():
-        #     if term in s:
-        #         return key
         return tags.findKey(term)
 
+    # This method will return the original string
     def getOrinalStr(self):
         return self.originalStr
 
+    # This method will return the non-tag terms
     def getUntaggedStr(self):
         return ' '.join(self.untaggedTerms)
 
+    # This method will return the tag terms
     def getTagStr(self):
         return ' '.join(self.taggedTerms)
 
+    # This method will return key-substituted string
     def getSubStr(self):
         return ' '.join(self.replacedStr)
-
-    # def processStr(self):
-    #     terms = self.originalStr.lower().split(' ')
-    #     for term in terms:
-    #         foundKey = self.getDictKey(term)
-    #         if foundKey:
-    #             self.taggedTerms.append(term)
-    #             self.replacedStr.append(foundKey)
-    #         else:
-    #             self.replacedStr.append(term)
-    #             self.untaggedTerms.append(term)
 
 myStr = Taggerizer("I am Jack and I am three years old")
 print myStr.getOrinalStr()
--- myTags.py
+++ myTags.py
@@ -1,7 +1,9 @@
+#Define an updateable dictionary.  This is according to requirement 1.1
 class myDict:
     def __init__(self):
         self.tagDict = {'NAME': set(['jack', 'jill']), 'NUM': set(['one', 'two', 'three', 'four', 'nine'])}
 
+    # Method to search for a term in the dictionary and return the key if found
     def findKey(self, term):
         for key, seet in self.tagDict.items():
             if term in seet:
