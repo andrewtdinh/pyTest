02bda0c9503e1d7c436755913ced83ecf61189ec
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-04 15:43:21 -0700

    Cannot get Python Selenium script to run.  Some kind of permission problem

--- PySelenium/testAWeber.py
+++ PySelenium/testAWeber.py
@@ -1,11 +1,13 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class AweberTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
+        binary = FirefoxBinary('/Library/Python/2.7/site-packages/selenium/webdriver/firefox/firefox_binary.py')
+        cls.driver = webdriver.Firefox(firefox_binary=binary)
 
     def test_title(self):
         self.driver.get('https://www.aweber.com')
