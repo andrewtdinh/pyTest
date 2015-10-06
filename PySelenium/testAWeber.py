86bce8b2d2dfefd7f75fcfeba935cf1000c3f77e
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-05 23:40:30 -0700

    Can get Firefox browser to open

--- PySelenium/testAWeber.py
+++ PySelenium/testAWeber.py
@@ -1,13 +1,13 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class AweberTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        binary = FirefoxBinary('/Library/Python/2.7/site-packages/selenium/webdriver/firefox/firefox_binary.py')
-        cls.driver = webdriver.Firefox(firefox_binary=binary)
+        # cls.driver = webdriver.Firefox()
+        cls.driver = webdriver.Chrome()
 
     def test_title(self):
         self.driver.get('https://www.aweber.com')
