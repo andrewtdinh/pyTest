e89ba44981bf28ac3b6f23c7b7c21aef4f05ee73
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-10 00:47:09 -0700

    Add unit tests for getting original string, untagged string, and tagged string.

--- myUnitTests.py
+++ myUnitTests.py
@@ -2,4 +2,15 @@ import unittest
 from myTagProcessor import Taggerizer
 
 class TestTagProcessor(unittest.TestCase):
-    def 
+    testObj = Taggerizer("I am Jack and I am three years old")
+    anotherObj = Taggerizer("  This is Jill's and Jack's sentence with leading and trailing spaces.   ")
+
+    def test_getOriginalStr(self):
+        self.assertEqual(testObj.getOrinalStr(), 'I am Jack and I am three years old')
+        self.assertEqual(anotherObj .getOrinalStr(), "  This is Jill's and Jack's sentence with leading and trailing spaces.   ")
+
+    def test_getUntaggedStr(self):
+        self.assertEqual(testObj.getUntaggedStr(), 'i and old am years')
+
+    def test_getTagStr(self):
+        self.assertEqual(testObj.getUntaggedStr(), 'I am Jack and I am three years old')
