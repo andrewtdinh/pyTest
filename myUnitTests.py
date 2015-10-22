d09e2de0877b5d261d6ff2c6bc0edaab8beb8bdb
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-21 23:31:39 -0700

    Check getTagStr method after removing a tag from dictionary

--- myUnitTests.py
+++ myUnitTests.py
@@ -51,7 +51,8 @@ class TestTagProcessor(unittest.TestCase):
         thirdObj.removeTagFromDict('jack')
         self.assertEqual(thirdObj.getOrinalStr(), "  This is Jill's and Jack's sentence, with leading and trailing spaces.   ")
         self.assertEqual(thirdObj.getUntaggedStr(), 'trailing sentence this leading is s spaces jack with')
-        print "\n" +thirdObj.getUntaggedStr()
+        self.assertEqual(thirdObj.getTagStr(), 'and jill')
+        print "\n" +thirdObj.getTagStr()
         # self.assertEqual(thirdObj.getTagStr(), 'and jill')
         # self.assertEqual(thirdObj.getSubStr(), "This is NAME's CONN NAME's sentence, with leading CONN trailing spaces.")
 
